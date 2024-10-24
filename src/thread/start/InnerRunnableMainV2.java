b3621c4e46ad4a3fd14d57392a2dc4246f5a28be
BiFoS <jon89071@gmail.com>
2024-10-24 16:41:01 +0900

    :memo: 1: 스레드를 생성하는 방법 공부

---
+++ src/thread/start/InnerRunnableMainV2.java
@@ -0,0 +1,17 @@
+package thread.start;
+
+import static util.MyLogger.log;
+
+public class InnerRunnableMainV2 {
+
+  public static void main(String[] args) {
+    log("main() start");
+
+    final Runnable runnable = () -> log("run()");
+    Thread thread = new Thread(runnable);
+    thread.start();
+
+    log("main() end");
+  }
+
+}
